81cffddb1fe62700f362c0157f65fc08972e7ea8
ulfetalhussaini <ulfetalhussaini@yahoo.com>
2023-11-01 16:42:07 +0300

    **Refactor**update myInput component

--- src/components/MyInput.js
+++ src/components/MyInput.js
@@ -1,46 +1,40 @@
 import React from "react";
 
 function MyInput({ Header, EditableRowData, SetEditableRowData }) {
+  const isNumeric =
+    Header === "age" || Header === "account_id" || Header === "amount";
+  const maxInputLength = {
+    age: 3,
+    account_id: 10,
+    amount: 10,
+    name: 20,
+  };
+
+  const handleInputChange = (e) => {
+    let inputValue = e.target.value;
+
+    if (isNumeric) {
+      // Allow only numeric input
+      inputValue = inputValue.replace(/[^0-9]/g);
+    }
+
+    if (inputValue.length > maxInputLength[Header]) {
+      // Limit the input to the specified maxLength
+      inputValue = inputValue.slice(0, maxInputLength[Header]);
+    }
+
+    SetEditableRowData({
+      ...EditableRowData,
+      [Header]: inputValue,
+    });
+  };
+
   return (
     <input
-      type={
-        Header === "age" || Header === "account_id" || Header === "amount"
-          ? "number"
-          : "text"
-      }
+      type={isNumeric ? "number" : "text"}
       value={EditableRowData[Header]}
-      onChange={(e) => {
-        if (
-          Header === "age" ||
-          Header === "account_id" ||
-          Header === "amount"
-        ) {
-          e.target.value = e.target.value.replace(/[^0-9]/g);
-        }
-        SetEditableRowData({
-          ...EditableRowData,
-          [Header]: e.target.value,
-        });
-      }}
-      onInput={(e) => {
-        if (
-          (Header === "age" && e.target.value.length > 3) ||
-          ((Header === "account_id" || Header === "amount") &&
-            e.target.value.length > 10) ||
-          (Header === "name" && e.target.value.length > 20)
-        ) {
-          e.target.value = e.target.value.slice(0, e.target.maxLength);
-        }
-      }}
-      maxLength={
-        Header === "age"
-          ? 3
-          : Header === "account_id" || Header === "amount"
-          ? 10
-          : Header === "name"
-          ? 20
-          : undefined
-      }
+      onChange={handleInputChange}
+      maxLength={maxInputLength[Header]}
     />
   );
 }
